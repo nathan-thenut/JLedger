38c21478864ad4e09efb04d159faf424a62a6d60
Nathan Eikermann <tensazangetsu1990@googlemail.com>
2019-09-12 21:20:49 +0200

    restructuring take two

---
+++ src/main/java/model/Ledger.java
@@ -0,0 +1,51 @@
+package jledger.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * This class represents a ledger file.
+ */
+public class Ledger {
+    
+    private String name;
+    private List<Account> accounts;    
+    private List<Transaction> transactions;
+
+    public Ledger(String name) {
+        this.name = name;
+        this.accounts = new ArrayList<>();
+        this.transactions = new ArrayList<>();
+    }
+
+    public Ledger(String name, List<Account> accounts, List<Transaction> transactions) {
+        this.name = name;
+        this.accounts = accounts;
+        this.transactions = transactions;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(List<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
+}
---
+++ src/main/java/model/Transaction.java
@@ -0,0 +1,113 @@
+package jledger.model;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Transaction {
+    
+    private Date date;
+    private String description;
+    private List<Posting> additions;
+    private List<Posting> removals;
+
+
+    public Transaction(Date transactionDate, String description) {
+        this.date = transactionDate;
+        this.description = description;
+        this.additions = new ArrayList<>();
+        this.removals = new ArrayList<>();
+    } 
+    
+    
+    public boolean checkTransactionBalance() {
+        
+        List<String> currenciesAdditions = this.additions.stream()
+            .map(p -> p.getCurrency())
+            .distinct()
+            .collect(Collectors.toList());
+
+        List<String> currenciesRemovals = this.removals.stream()
+            .map(p -> p.getCurrency())
+            .distinct()
+            .collect(Collectors.toList());
+
+        if (!new HashSet<>(currenciesAdditions).equals(new HashSet<>(currenciesRemovals))) {
+            //TODO: Debug msg?
+            return false;
+        }
+
+
+        for (String currency : currenciesAdditions) {
+
+            BigDecimal amountAdditions = this.additions.stream()
+                .filter(p -> p.getCurrency().equals(currency))
+                .map(p -> p.getAmount())
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+            BigDecimal amountRemovals = this.removals.stream()
+                .filter(p -> p.getCurrency().equals(currency))
+                .map(p -> p.getAmount())
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+            
+            if (amountAdditions.add(amountRemovals).compareTo(new BigDecimal("0")) != 0) {
+                //TODO: Debug msg?
+                return false;
+            }
+        }
+        
+
+        return true;
+    }
+    
+    
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+
+    public List<Posting> getAdditions() {
+        return additions;
+    }
+
+    public void setAdditions(List<Posting> additions) {
+        this.additions = additions;
+    }
+
+    public void addAdditions(List<Posting> newAdditions) {
+        this.additions.addAll(newAdditions);
+    }
+
+    public List<Posting> getRemovals() {
+        return removals;
+    }
+
+    public void setRemovals(List<Posting> removals) {
+        this.removals = removals;
+    }
+
+    public void addRemovals(List<Posting> newRemovals) {
+        this.removals.addAll(newRemovals);
+    }
+
+    public List<Posting> getAllPostings() {
+        return Stream.concat(this.additions.stream(), this.removals.stream())
+            .collect(Collectors.toList());
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
