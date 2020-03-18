1e2bc45ddb50d195d31cb29b426bf61253a838ac
Nathan Eikermann <tensazangetsu1990@googlemail.com>
2020-03-18 18:14:52 +0100

    inital commit for parser

---
+++ src/main/java/parser/Parser.java
@@ -0,0 +1,15 @@
+package jledger.parser;
+
+/*
+ * This class represents a parser for 
+ */
+public class Parser {
+    
+    public Parser() {
+
+    }
+
+    public String readFile(String filename) {
+        return "";
+    }
+}
---
+++ src/test/java/parser/ParserTest.java
@@ -0,0 +1,14 @@
+package jledger.parser;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ParserTest {
+   
+    @Test
+    public void testReadingFile() {
+        assertFalse(true, "Test");
+        test();
+    }
+
+}
